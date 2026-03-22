5056594afaca018758d27451946b2db62bf6db0a
dkasha14 <dlrashadk@gmail.com>
2026-03-22 17:56:27 +0530

    Configure root module to orchestrate s3, vpc, and ec2 modules

--- main.tf
+++ main.tf
@@ -1,31 +1,60 @@
-# working with modules
-
 module "storage" {
-	source = "./s3"
-	mys3bucket_name = "dvs-devops1"
-
+  source = "./s3"
+  mys3bucket_name = "dk-devop-bucket"
 }
 
 module "network" {
+  source = "./vpc"
 
-	source = "./vpc"
-	myvpc_name = "dvs-vpc"
-	myvpc_cidr = "10.124.0.0/16"
-	myigw_name = "dvsigw"
-	myroute_name = "dvsroute"
-	mypubsubnet_name = "dvspubsubnet"
-	mypubsubnet_cidr = "10.124.10.0/24"
-	mysecgroup_name	= "dvssg"
-
+  myvpc_name        = "dk-vpc"
+  myvpc_cidr        = "10.124.0.0/16"
+  myigw_name        = "dk-igw"
+  myroute_name      = "dk-route"
+  mypubsubnet_name  = "dk-pubsubnet"
+  mypubsubnet_cidr  = "10.124.10.0/24"
+  mysecgroup_name   = "dk-secgroup"
 }
 
 module "compute" {
-	source = "./ec2"
-	myserver_name = "DvsPubServer"
-	keyname = "rajukey_nvirginia"
-	instance_type = "t2.micro"
-	vpcsgid = "${module.network.mysgid}"
-	subnetid = "${module.network.mysubnetid}"
+  source = "./ec2"
 
+  myserver_name = "DK-PubServer"
+  keyname       = "asha.nvirg"
+  instance_type = "t2.micro"
+
+  vpcsgid = module.network.mysgid
+  subnetid = module.network.mysubnetid
 }
+
+
+ # working with modules
+
+# module "storage" {
+# 	source = "./s3"
+# 	mys3bucket_name = "dk-devop-bucket"
+
+# }
+
+# module "network" {
+
+# 	source = "./vpc"
+# 	myvpc_name = "dk-vpc"
+# 	myvpc_cidr = "10.124.0.0/16"
+# 	myigw_name = "dk-igw"
+# 	myroute_name = "dk-route"
+# 	mypubsubnet_name = "dk-pubsubnet"
+# 	mypubsubnet_cidr = "10.124.10.0/24"
+# 	mysecgroup_name	= "dk-secgroup"
+
+# }
+
+# module "compute" {
+# 	source = "./ec2"
+# 	myserver_name = "DK-PubServer"
+# 	keyname = "asha.nvirg"
+# 	instance_type = "t2.micro"
+# 	vpcsgid = "${module.network.mysgid}"
+# 	subnetid = "${module.network.mysubnetid}"
+
+# }
 
